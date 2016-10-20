3bf5d9c82360eaa4a5bebe970be5492f0b512663
Jiar <jiar.world@gmail.com>
2016-10-20 15:16:37 +0800

    添加默认入口

--- index.php
+++ index.php
@@ -21,6 +21,10 @@ define('APP_DEBUG',True);
 // 定义应用目录
 define('APP_PATH','./Application/');
 
+$_GET['m'] = 'Qwechat';
+$_GET['c'] = 'Qwechat';
+$_GET['a'] = 'qwechat';
+
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
