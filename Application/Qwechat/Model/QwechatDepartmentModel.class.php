c1dc4c07c6ded82a34699aa14e6f1299fc196248
Jiar <jiar.world@gmail.com>
2016-10-25 16:45:57 +0800

    debug

--- Application/Qwechat/Model/QwechatDepartmentModel.class.php
+++ Application/Qwechat/Model/QwechatDepartmentModel.class.php
@@ -33,7 +33,7 @@ class QwechatDepartmentModel extends Model {
     $department = D('QwechatDepartment')->select();
     if(count($department) == 0) {
       $department = $this->getDepartmentFromQwechat();
-      $department = json_decode($department,true);
+      // $department = json_decode($department,true);
       // $department = $department['department'];
     }
     return $department;
