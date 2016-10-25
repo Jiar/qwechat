bd97a6433d3fcd5b2f7a0e9e6e388eed9877bf19
Jiar <jiar.world@gmail.com>
2016-10-25 10:34:57 +0800

    modify

--- Application/Qwechat/Conf/config.php
+++ Application/Qwechat/Conf/config.php
@@ -9,13 +9,13 @@ return array(
     'DB_PORT' => '3306',
     'DB_PREFIX' => 'db_',
 
-    // 'DB_TYPE' => 'mysql',
-    // 'DB_HOST' => '127.0.0.1',
-    // 'DB_USER' => 'root',
-    // 'DB_PWD' => 'palcomm007',
-    // 'DB_NAME' => 'pal_qwechat',
-    // 'DB_PORT' => '3306',
-    // 'DB_PREFIX' => 'db_',
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'palcomm007',
+    'DB_NAME' => 'pal_qwechat',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'db_',
 
 	'app_begin' => array('Behavior\CheckLangBehavior'),
 
