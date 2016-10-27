aea57af94ae5979f250d1f2deab5388c27b148da
Jiar <jiar.world@gmail.com>
2016-10-27 23:06:06 +0800

    modify

--- Application/Qwechat/Conf/config.php
+++ Application/Qwechat/Conf/config.php
@@ -17,8 +17,6 @@ return array(
     'DB_PORT' => '3306',
     'DB_PREFIX' => 'db_',
 
-	// 'app_begin' => array('Behavior\CheckLangBehavior'),
-
     'LANG_SWITCH_ON'   => true,
     'LANG_AUTO_DETECT' => true, 		 // 自动侦测语言 开启多语言功能后有效
 	'LANG_LIST'        => 'zh-cn,en-us', // 允许切换的语言列表 用逗号分隔
