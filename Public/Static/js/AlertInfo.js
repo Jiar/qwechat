0efd6a30ce6de38416214df1464391fc2e580af0
Jiar <jiar.world@gmail.com>
2016-10-31 12:41:49 +0800

    debug

---
+++ Public/Static/js/AlertInfo.js
@@ -0,0 +1,23 @@
+/**
+ * AlertInfo
+ */
+var AlertInfo = (function() {
+    "use strict";
+
+    var elem,
+        hideHandler,
+        that = {};
+
+    that.init = function(options) {
+        elem = $(options.selector);
+    };
+
+    that.show = function(text) {
+        clearTimeout(hideHandler);
+
+        elem.find("span").html(text);
+        elem.delay(200).fadeIn().delay(4000).fadeOut();
+    };
+
+    return that;
+}());
